f088867507460b4131dca2bca4565ac7e0ca53b6
alisidd1 <alisiddiqui@gmail.com>
2024-01-28 18:38:41 -0500

    first commit

---
+++ CalculatorNew/CalcCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CalcCollectionViewCell.swift
+//  CalculatorNew
+//
+//  Created by Ali Siddiqui on 1/28/24.
+//
+
+import UIKit
+
+class CalcCollectionViewCell: UICollectionViewCell {
+    
+}
